6e79b2f4630db2ed3b390ae9945e7b4f42dae9e4
Antu-code <jibonoshomapto3@gmail.com>
2022-02-24 01:08:20 -0800

    antu

--- script.js
+++ script.js
@@ -13,15 +13,15 @@ file.oninput = () =>{
 
 document.getElementById('open-popup-btn').addEventListener('click',function(){document.getElementsByClassName('popup')[0].classList.add('active');
 });
-document.getElementById('dismiss-popup-btn').addEventListener('click',function(){document.getElementsByClassName('popup')[0].classList.remove('active');
-});
+// document.getElementById('dismiss-popup-btn').addEventListener('click',function(){document.getElementsByClassName('popup')[0].classList.remove('active');
+// });
 
 /************PROGRESS-BAR-JS********** */
 const progress = document.querySelector('.progress-bar');
 setTimeout(()=>{
     progress.style.width = progress.getAttribute('data-done') + '%';
     progress.style.opacity = 1;
-},1500)
+},100)
     
 
 
@@ -29,6 +29,13 @@ setTimeout(()=>{
 
 
 /******START-SECOUND-SECTION-JS******/
+document.getElementById('open-popup-btnn').addEventListener('click',function(){document.getElementsByClassName('popup-two')[0].classList.add('active');
+});
+const progresss = document.querySelector('.progress-bar-two');
+setTimeout(()=>{
+    progresss.style.width = progresss.getAttribute('data-done') + '%';
+    progresss.style.opacity = 1;
+},100)
 
 /****************NUMBER-INPUT******************/
 
@@ -50,3 +57,9 @@ if(btn == 10){
 
 document.getElementById('open-popup-btnn').addEventListener('click',function(){document.getElementsByClassName('popupp')[0].classList.add('activee');
 });
+/************PROGRESS-BAR-JS********** */
+// const progresss = document.querySelector('.progress-barr');
+// setTimeout(()=>{
+//     progresss.style.width = progresss.getAttribute('data-done') + '%';
+//     progresss.style.opacity = 1;
+// },1500)
